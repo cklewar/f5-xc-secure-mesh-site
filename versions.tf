eadfe089182262ec10deadcc8024791ea7ad3080
cklewar <cklewar@t-online.de>
2023-05-03 02:18:07 +0200

    0.11.21 v26

--- versions.tf
+++ versions.tf
@@ -26,6 +26,14 @@ terraform {
       source  = "hashicorp/vsphere"
       version = ">= 2.3.1"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.25.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.48.0"
+    }
     local = ">= 2.2.3"
     null  = ">= 3.1.1"
   }
