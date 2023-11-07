bb246f7c4a7debd863109649b31b67328c5f374e
cklewar <cklewar@t-online.de>
2023-11-07 17:34:27 +0100

    Launch 0.11.23

--- versions.tf
+++ versions.tf
@@ -8,33 +8,33 @@ terraform {
       name = "f5-xc-secure-mesh-site-module"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = "= 0.11.21"
+      source = "volterraedge/volterra"
+      version = "= 0.11.23"
     }
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = ">= 4.51.0"
     }
     restapi = {
-      source  = "Mastercard/restapi"
+      source = "Mastercard/restapi"
       version = "1.18.0"
     }
     vsphere = {
-      source  = "hashicorp/vsphere"
+      source = "hashicorp/vsphere"
       version = ">= 2.3.1"
     }
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = ">= 3.25.0"
     }
     google = {
-      source  = "hashicorp/google"
+      source = "hashicorp/google"
       version = ">= 4.48.0"
     }
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
